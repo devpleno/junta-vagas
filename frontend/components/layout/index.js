cc0b7a09d64ffbb3a301969a8c7ebcd6de3f9a3a
Carlos Nascimento <acntonycar62@gmail.com>
2022-03-07 21:04:37 -0300

    screen send email

---
+++ frontend/components/layout/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "../footer"
+import Header from "../header"
+
+
+const Layout = ({children}) => {
+    return (
+      <div>
+            <Header />
+            <div className="container mx-auto">
+                {children} 
+            </div>
+            <Footer/>
+      </div>  
+
+    )
+}
+
+export default Layout
---
+++ frontend/pages/email.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Link from "next/link";
+
+
+const Email = () => {
+    return(
+    <div class="mt-1">          
+            <div>
+                <Link href='/'>
+                    <a></a>
+                </Link>              
+            </div>
+
+            <div class="w-full h-screen flex items-center justify-center">
+        <form class="w-full md:w-1/3 bg-white rounded-lg">
+            <div class="flex font-bold justify-center mt-6">
+                <img class="h-20 w-20"
+                    src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/avatar.svg"/>
+            </div>
+            <h2 class="text-3xl text-center text-gray-700 mb-4">Informe seu Email, Receber vaga.</h2>
+            <div class="px-12 pb-10">
+                <div class="w-full mb-2">
+                    <div class="flex items-center">
+                        <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-user'></i>
+                        <input type='text' placeholder="Email"
+                            class="-mx-6 px-8  w-full border rounded px-3 py-2 text-gray-700 focus:outline-none" />
+                    </div>
+                </div>
+                <div class="w-full mb-2">
+                    <div class="flex items-center">
+                        <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock'></i>
+                        <input type='text' placeholder="Senha"
+                            class="-mx-6 px-8 w-full border rounded px-3 py-2 text-gray-700 focus:outline-none" />
+                    </div>
+                </div>               
+                <button type="submit"
+                    class="w-full py-2 rounded-full bg-green-600 text-gray-100  focus:outline-none">Enviar</button>
+            </div>        
+        </form>
+    </div>
+
+     </div>   
+  )
+}
+
+export default Email
