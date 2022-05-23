fe95636b560d6f5cb830943e157aea86dc57e7d6
Carlos Nascimento <acntonycar62@gmail.com>
2022-05-23 20:49:15 -0300

    environment variable

--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -1,7 +1,15 @@
 import React from "react"
 
 const Home = () => {
-    return
+    return(
+        <div class="mt-1">          
+            <div>
+                <Link href='/'>
+                    <a></a>
+                </Link>              
+            </div>
+        </div>    
+    )
 }
 
 export default Home
