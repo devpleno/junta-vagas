6a702a83e2573e67eafc4acc8b8935cd755a2fc5
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-06-11 12:27:03 -0300

    Changes to suitable page the invite of discord

--- frontend/pages/convite.js
+++ frontend/pages/convite.js
@@ -1,26 +1,17 @@
 import React from "react";
-import Link from "next/link";
 
 const Convite = () => {
-      return (
-        <main className="h-screen w-full bg-gray-200 text-center flex flex-col">
-        <Link href='/'>
-            <a></a>
-        </Link>              
-   
-        <div className='w-full h-screen flex  items-center justify-center'>       
-             <h1 className="container text-3xl font-bold
-              text-blue-300 self-center py-20 md:text-6xl"> Link Discord <br /></h1>             
-            <h2 className="container text-2xl font-bold
-              text-black self-center  md:text-6xl" >                
-                 <a href={ process.env.NEXT_PUBLIC_LINK_DISCORD } target='_blank'>
-                    {process.env.NEXT_PUBLIC_LINK_DISCORD}
-                 </a> 
-            </h2>
-            
-           
-        </div>
+
+  return (
+    <main className="h-screen w-full bg-gray-200 text-center flex flex-col">
+      <div className='w-full h-screen flex  items-center justify-center'>
+          <a className="container text-3xl font-bold
+              text-blue-300 self-center py-20 md:text-6xl" 
+              href={process.env.NEXT_PUBLIC_API_URL_NEWSLETTER} target='_blank'>
+            Link para acessar o Discord
+          </a>
+      </div>
     </main>
-    )
+  )
 }
 export default Convite
