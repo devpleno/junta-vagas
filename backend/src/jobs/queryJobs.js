7ac47088a3267567c83c81a1dc307b20bb808d6e
Wilian Ansanello <wiliansanello@outlook.com>
2022-06-18 11:11:41 -0300

    the function queryJobs has been fixed to use the db methods

--- backend/src/jobs/queryJobs.js
+++ backend/src/jobs/queryJobs.js
@@ -1,13 +1,11 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-const dotenv = require('dotenv/config')
+require("dotenv").config()
+
+const { connect, client: connectionDB, findOne } = require("../services/db")
 
 const getToday = () => {
   const today = new Date()
   return today.getFullYear() + '-' + 0 + (today.getMonth() + 1) + '-' + today.getDate()
 }
-const initialHour = 'T00:00:00.000Z'
-const finalHour = 'T23:59:59.999Z'
 
 const queryDate = getToday()
 
@@ -15,8 +13,7 @@ const queryJobs = async coll => {
   const cursorJobs = await coll.find(
     {
       "postedAt": {
-        $gte: queryDate + initialHour,
-        $lte: queryDate + finalHour
+        $gte: `${queryDate} 00:00:00`
       }
     }).toArray()
 
@@ -24,25 +21,14 @@ const queryJobs = async coll => {
   return cursorJobs
 }
 
-const connectDb = new Promise((resolve, reject) => {
-  const connAdr = 'mongodb://' + process.env.URL
-    + ':' + process.env.PORT
-    + '/' + process.env.COLLECTION
-  MongoClient.connect(connAdr, (err, client) => {
-    if (err) {
-      return reject(err)
-    }
-    const db = client.db(process.env.DATABASE)
-    resolve({ db: db, client: client })
-  })
-})
-
 module.exports = {
-  queryData: () => {
-    connectDb.then(queryData = async data => {
-      const jobsDb = data.db.collection(process.env.COLLECTION)
-      await queryJobs(jobsDb)
-      data.client.close()
-    })
+  queryData: async (connectionDB) => {
+    const db = connectionDB.db(process.env.DB_NAME)
+    const collection = db.collection("jobs")
+    connect()
+      .then(async () => {
+        findOne('jobs', queryJobs)
+      })
+    return queryJobs(collection)
   }
 }
