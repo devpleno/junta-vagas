6b9b5d52a59bc2415cdd74071b0f3fc7d2df9e69
Wilian Ansanello <75302006+wiliansanello@users.noreply.github.com>
2022-09-08 16:43:03 -0300

    improvements to get API URL in frontend

--- frontend/pages/email.js
+++ frontend/pages/email.js
@@ -27,13 +27,13 @@ function onChangeEmail (evt) {
     setEmail(evt.target.value);
 }
 
-async function getEmailFromForm (evt) {
+async function registerEmailInNewsletter (evt) {
     let status = 0
     let message = '';
     evt.preventDefault();
     try {
         const res = await axios.post(
-            "http://localhost:3000/newsletter/register", {
+            `${process.env.NEXT_PUBLIC_API_URL}/newsletter/register`, {
                 'email': email
             }                       
         );
@@ -48,35 +48,35 @@ async function getEmailFromForm (evt) {
 }
 
     return(
-    <div class="mt-1">          
+    <div className="mt-1">          
             <div>
                 <Link href='/'>
                     <a></a>
                 </Link>              
             </div>
 
-            <div class="w-full h-screen flex items-center justify-center">
-        <form class="w-full md:w-1/3 bg-white rounded-lg" onSubmit={getEmailFromForm}>
-            <div class="flex font-bold justify-center mt-6">
-                <img class="h-20 w-20"
+            <div className="w-full h-screen flex items-center justify-center">
+        <form className="w-full md:w-1/3 bg-white rounded-lg" onSubmit={registerEmailInNewsletter}>
+            <div className="flex font-bold justify-center mt-6">
+                <img className="h-20 w-20"
                     src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/avatar.svg"/>
             </div>
-            <h2 class="text-3xl text-center text-gray-700 mb-4">Informe seu Email, Receber vaga.</h2>
-            <div class="px-12 pb-10">
-                <div class="w-full mb-2">
-                    <div class="flex items-center">
-                        <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-user'></i>
+            <h2 className="text-3xl text-center text-gray-700 mb-4">Informe seu Email, Receber vaga.</h2>
+            <div className="px-12 pb-10">
+                <div className="w-full mb-2">
+                    <div className="flex items-center">
+                        <i className='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-user'></i>
                         <input type='text' placeholder="Email" name="email" value={email} onChange={onChangeEmail}
                             class="-mx-6 px-8  w-full border rounded px-3 py-2 text-gray-700 focus:outline-none" />
                     </div>
                 </div>
-                <div class="w-full mb-2">
-                    <div class="flex items-center">
-                        <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock'></i>
+                <div className="w-full mb-2">
+                    <div className="flex items-center">
+                        <i className='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock'></i>
                     </div>
                 </div>               
                 <button type="submit"
-                    class="w-full py-2 rounded-full bg-green-600 text-gray-100  focus:outline-none">Enviar</button>
+                    className="w-full py-2 rounded-full bg-green-600 text-gray-100  focus:outline-none">Enviar</button>
             </div>                   
         </form>
     </div> 
