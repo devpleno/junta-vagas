db7751abc5e96d0987553ef7a9380d99f94c6de4
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-12-09 01:09:17 -0300

    Changes the frontend

--- frontend/components/footer/index.js
+++ frontend/components/footer/index.js
@@ -7,9 +7,9 @@ const Footer = () => {
         <div className='bg-gray-700 px-4'>
             <div className='container mx-auto text-center font-bold text-white px-4'>  
                 Projeto desenvolvido por:
-                equipe do juntas Vagas <br />
-                <a href='www.linkedin.com/in/carlosnasciemnto'>Linkedin</a>/
-                <a href='https://github.com/Carlos-Nasciemnto'>Github</a>                
+                equipe do juntas Vagas
+                 <br />
+                <a href="https://github.com/devpleno/junta-vagas">Clique aqui para acessar Github do projeto</a>
             </div>          
             <div className='container mx-auto text-center min-h-full'>
             <img className='inline p-4" src="https://img.icons8.com/external-filled-outline-icons-maxicons/85/000000/external-browser-type-of-website-filled-outline-filled-outline-icons-maxicons-23.png'/>
--- frontend/components/layout/index.js
+++ frontend/components/layout/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Footer from "../footer"
 import Header from "../header"
+import Head from 'next/head'
 
 
 const Layout = ({children}) => {
     return (
       <div>
+            <Head>
+              <title>Junta vagas - Onde encontra sua vaga de TI</title>
+              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+              <meta name="title" content="Junta vagas - Onde encontra sua vaga de TI" />
+                <meta name="description" content="Vagas de T.I em um único lugar, vagas nas tecnologias: php, java, js, javascript, python, ruby, golang, go, elixir, laravel, springboot, vraptor, node..js, express.js, nest.js, next.js, phonex, Django, ruby on rails, react.js, react, react native, angular, vue e vue.js" />
+                <meta name="robots" content="index, follow" />
+                <meta http-equiv="content-language" content="pt-br" />
+            </Head>
             <Header />
             <div className="container mx-auto">
                 {children} 
