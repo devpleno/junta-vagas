b4564548ece6fb82cf34ca3d9940befe876a0991
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-12-09 20:06:13 -0300

    Changes to resolve crawler scraping site Startec

--- backend/src/crawlers/extractJobsSiteStartec.js
+++ backend/src/crawlers/extractJobsSiteStartec.js
@@ -1,28 +1,29 @@
 const axios = require("axios")
 
 async function getJobs() {
-  let response = await axios.get("https://startec-api-production.up.railway.app/v2/jobs/list/all")
+  let response = await axios.get("https://startec-candidate-api-production.up.railway.app/jobs")
   let vagas = response.data;
   const todayDate = new Date();
   todayDate.setHours(0)
   todayDate.setMinutes(0)
   todayDate.setSeconds(0)
 
-  var job = vagas.jobs.filter(job => {
+  var job = vagas.data.filter(job => {
     return new Date(job.created_at).getTime() >= todayDate.getTime() 
   }).map((job) => {
 
-    const skills = job.jobSkills.map(skill => {
+    const skills = job.skills.map(skill => {
       return skill.name
     })
 
     return {
-      title: job.title,
-      company: job.companyName,
-      locate: job.workModel,
+      title: job.name,
+      company: job.company.name,
+      locate: job.model,
       requirements: skills,
-      postedAt: job.created_at,
-      logo: job.companyLogo
+      postedAt: job.createdAt,
+      logo: job.company.avatarSrc,
+      link: `https://startecjobs.com/jobs/${job.id}`
     }
   })
 
